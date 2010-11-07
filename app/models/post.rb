c63506acc5d97231d4f448c65d940094319abce1
Christopher Bradford <bradfordcp@gmail.com>
2010-11-07 18:49:00 -0500

    Basic solr-ruby implementation

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,4 +3,24 @@ class Post < ActiveRecord::Base
   has_and_belongs_to_many :tags
   
   validates_presence_of :title, :body
+  
+  # Generates a hash to send to the solr server using the solr-ruby gem
+    def to_solr
+      sol = {}
+
+      sol[:id] = "#{self.class}:#{self.id}"
+      sol[:type] = self.class
+      sol[:title] = self.title
+      sol[:category] = self.category.name
+      sol[:body] = self.body
+      sol[:tidy_body] = HTML::FullSanitizer.new.sanitize(self.description)[0..250]
+      
+      tags = []
+      self.tags.each do |tag|
+        tags << tag.name
+      end
+      sol[:tags] = tags
+
+      return sol
+    end
 end
